49ac2881b378935dd539d27b0c4197f076a96df6
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-27 10:13:45 -0700

    Update new-animal component format.

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -8,40 +8,48 @@ import { Animal } from './animal.model';
     <h2>Add an Animal</h2>
     <form>
       <div class="form-group">
-        <label for="newSpecies">Species:</label>
-        <input #newSpecies class="form-control">
-      </div>
-      <div class="form-group">
         <label for="newName">Name:</label>
         <input #newName class="form-control">
       </div>
-      <div class="form-group">
-        <label for="newAge">Age:</label>
-        <input #newAge class="form-control">
+      <div class="row">
+        <div class="form-group col-sm-6">
+          <label for="newSpecies">Species:</label>
+          <input #newSpecies class="form-control">
+        </div>
+        <div class="form-group col-sm-6">
+          <label for="newDiet">Diet:</label>
+          <input #newDiet class="form-control">
+        </div>
       </div>
-      <div class="form-group">
-        <label for="newDiet">Diet:</label>
-        <input #newDiet class="form-control">
+      <div class="row">
+        <div class="form-group col-sm-6">
+          <label for="newLocation">Location:</label>
+          <input #newLocation class="form-control">
+        </div>
+        <div class="form-group col-sm-6">
+          <label for="newCaretakers">Caretakers Needed:</label>
+          <input #newCaretakers class="form-control">
+        </div>
       </div>
-      <div class="form-group">
-        <label for="newLocation">Location:</label>
-        <input #newLocation class="form-control">
+      <div class="row">
+        <div class="form-group col-sm-6">
+          <label for="newAge">Age:</label>
+          <input #newAge class="form-control">
+        </div>
+        <div class="form-group col-sm-6">
+          <label for="newSex">Sex:</label>
+          <input #newSex class="form-control">
+        </div>
       </div>
-      <div class="form-group">
-        <label for="newCaretakers">Number of Caretakers:</label>
-        <input #newCaretakers class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="newSex">Sex:</label>
-        <input #newSex class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="newLikes">Likes:</label>
-        <input #newLikes class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="newDislikes">Dislikes:</label>
-        <input #newDislikes class="form-control">
+      <div class="row">
+        <div class="form-group col-sm-6">
+          <label for="newLikes">Likes:</label>
+          <input #newLikes class="form-control">
+        </div>
+        <div class="form-group col-sm-6">
+          <label for="newDislikes">Dislikes:</label>
+          <input #newDislikes class="form-control">
+        </div>
       </div>
       <button type="button" class="btn"
       (click)="add(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes); newDescription.value='';">
