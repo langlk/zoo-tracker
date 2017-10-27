eb5dd4882f130f0beab44487c5eaa1aa02b24c02
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-27 12:12:28 -0700

    Add responsive formatting to new animal form.

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -5,57 +5,59 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'new-animal',
   template: `
-    <h2>Add an Animal</h2>
-    <form>
-      <div class="form-group">
-        <label for="newName">Name:</label>
-        <input #newName class="form-control">
-      </div>
-      <div class="row">
-        <div class="form-group col-sm-6">
-          <label for="newSpecies">Species:</label>
-          <input #newSpecies class="form-control">
+    <div class="animal-form">
+      <h2>Add an Animal</h2>
+      <form>
+        <div class="form-group">
+          <label for="newName">Name:</label>
+          <input #newName class="form-control">
         </div>
-        <div class="form-group col-sm-6">
-          <label for="newDiet">Diet:</label>
-          <input #newDiet class="form-control">
+        <div class="row">
+          <div class="form-group col-xs-6">
+            <label for="newSpecies">Species:</label>
+            <input #newSpecies class="form-control">
+          </div>
+          <div class="form-group col-xs-6">
+            <label for="newDiet">Diet:</label>
+            <input #newDiet class="form-control">
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="form-group col-sm-6">
-          <label for="newLocation">Location:</label>
-          <input #newLocation class="form-control">
+        <div class="row">
+          <div class="form-group col-xs-6">
+            <label for="newLocation">Location:</label>
+            <input #newLocation class="form-control">
+          </div>
+          <div class="form-group col-xs-6">
+            <label for="newCaretakers">Caretakers Needed:</label>
+            <input #newCaretakers class="form-control">
+          </div>
         </div>
-        <div class="form-group col-sm-6">
-          <label for="newCaretakers">Caretakers Needed:</label>
-          <input #newCaretakers class="form-control">
+        <div class="row">
+          <div class="form-group col-xs-6">
+            <label for="newAge">Age:</label>
+            <input #newAge class="form-control">
+          </div>
+          <div class="form-group col-xs-6">
+            <label for="newSex">Sex:</label>
+            <input #newSex class="form-control">
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="form-group col-sm-6">
-          <label for="newAge">Age:</label>
-          <input #newAge class="form-control">
+        <div class="row">
+          <div class="form-group col-xs-6">
+            <label for="newLikes">Likes:</label>
+            <input #newLikes class="form-control">
+          </div>
+          <div class="form-group col-xs-6">
+            <label for="newDislikes">Dislikes:</label>
+            <input #newDislikes class="form-control">
+          </div>
         </div>
-        <div class="form-group col-sm-6">
-          <label for="newSex">Sex:</label>
-          <input #newSex class="form-control">
-        </div>
-      </div>
-      <div class="row">
-        <div class="form-group col-sm-6">
-          <label for="newLikes">Likes:</label>
-          <input #newLikes class="form-control">
-        </div>
-        <div class="form-group col-sm-6">
-          <label for="newDislikes">Dislikes:</label>
-          <input #newDislikes class="form-control">
-        </div>
-      </div>
-      <button type="button" class="btn"
-      (click)="add(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes); newDescription.value='';">
-        Submit
-      </button>
-    </form>
+        <button type="button" class="btn"
+        (click)="add(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes); newDescription.value='';">
+          Submit
+        </button>
+      </form>
+    </div>
   `
 })
 
